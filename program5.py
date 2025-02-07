f813123c1e02d39829866e6f426abe79da5131ef
Mayura Varma Jain <131279586+Mayuravarmajain@users.noreply.github.com>
2025-02-07 09:08:24 +0530

    Delete program5.py

--- program5.py
+++
@@ -1,5 +0,0 @@
-# find entered number is grater than or not
-a=int(input("Enter the value of the A is ::"))
-b=int(input("Enter the value of the B is ::"))
-
-print("A is the grate than the B is  ::", a > b)
