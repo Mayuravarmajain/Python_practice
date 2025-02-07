29fe0dfb53985abbf07fb05a9a978299536e95b4
Mayura Varma Jain <131279586+Mayuravarmajain@users.noreply.github.com>
2025-02-07 09:08:36 +0530

    Delete program6.py

--- program6.py
+++
@@ -1,7 +0,0 @@
-# find the avrage of the two entered number 
-
-a=int(input("Enter the value of the A is ::"))
-b=int(input("Enter the value of the B is ::"))
-
-
-print ("avrage of entered two number is :: ", (a+b)/2)
